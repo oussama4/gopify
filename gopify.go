1ca5bdfe7f089fc0c0e2f130f3356c5e6d129b6c
osama zowaki <oussamazouaki4@gmail.com>
2021-09-26 17:51:23 +0100

    make verify request an http middleware

--- gopify.go
+++ gopify.go
@@ -5,6 +5,7 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"errors"
+	"net/http"
 	"net/url"
 )
 
@@ -21,23 +22,29 @@ type Gopify struct {
 }
 
 // VerifyRequest verifies the authenticity of the request from Shopify
-func (g *Gopify) VerifyRequest(u url.URL) error {
-	q := u.Query()
-	mac, err := hex.DecodeString(q.Get("hmac"))
-	if err != nil {
-		return err
-	}
-	q.Del("hmac")
-	message, err := url.QueryUnescape(q.Encode())
-	if err != nil {
-		return err
-	}
+func (g *Gopify) VerifyRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		q := r.URL.Query()
+		mac, err := hex.DecodeString(q.Get("hmac"))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		q.Del("hmac")
+		message, _ := url.QueryUnescape(q.Encode())
 
+		if !g.ValidHmac(mac, message) {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+// ValidHmac validates the provided hmac value against the hmac value of the provided message
+func (g *Gopify) ValidHmac(mac []byte, message string) bool {
 	hasher := hmac.New(sha256.New, []byte(g.ApiSecret))
 	hasher.Write([]byte(message))
 	validMac := hasher.Sum(nil)
-	if !hmac.Equal(mac, validMac) {
-		return ErrUnauthorizedRequest
-	}
-	return nil
+	return hmac.Equal(mac, validMac)
 }
