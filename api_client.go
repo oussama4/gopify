fd633fc78503b36dfe0224e7b179de468556e57d
osama zowaki <oussamazouaki4@gmail.com>
2021-10-10 23:33:52 +0100

    handle api rate limit

--- api_client.go
+++ api_client.go
@@ -3,37 +3,87 @@ package gopify
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 )
 
+const (
+	defaultApiVersion = "2021-10"
+	defaultTimeout    = 10 * time.Second
+	defaultRetries    = 2
+)
+
+var (
+	ErrRateLimit = errors.New("API rate limit exeeded")
+)
+
+// Option is for configuring the Api client
+type Option func(c *Client)
+
+// WithVersion sets the Api version
+func WithVersion(version string) Option {
+	return func(c *Client) {
+		c.version = version
+	}
+}
+
+// WithTimeout sets a custom http timeout
+func WithTimeout(seconds int) Option {
+	return func(c *Client) {
+		c.client.Timeout = time.Duration(seconds) * time.Second
+	}
+}
+
+// WithRetries tells the Api client how many retries to perform when hit a rate limit
+func WithRetry(tries int) Option {
+	return func(c *Client) {
+		c.tries = tries
+	}
+}
+
+// Body is an API request/response body
+type Body map[string]interface{}
+
 // shopify API client
 type Client struct {
-	domain      string
-	baseUrl     string
-	accessToken string
-	version     string
-	client      *http.Client
+	client         *http.Client
+	domain         string
+	baseUrl        string
+	accessToken    string
+	version        string
+	tries          int
+	availableLimit int // used for handling rate limits
 }
 
-func NewClient(domain, accessToken, version string) *Client {
-	baseUrl := fmt.Sprintf("https://%s/admin/api/%s", domain, version)
+// Create a new shopify Api client
+// the domain parameter is the shop domain
+func NewClient(domain, accessToken string, opts ...Option) *Client {
+	baseUrl := fmt.Sprintf("https://%s/admin/api/%s", domain, defaultApiVersion)
 	client := http.Client{
-		Timeout: time.Second * 10,
+		Timeout: defaultTimeout,
 	}
-	api := &Client{
-		domain:      domain,
-		baseUrl:     baseUrl,
-		accessToken: accessToken,
-		version:     version,
-		client:      &client,
+	c := &Client{
+		client:         &client,
+		domain:         domain,
+		baseUrl:        baseUrl,
+		accessToken:    accessToken,
+		version:        defaultApiVersion,
+		tries:          defaultRetries,
+		availableLimit: 0,
 	}
 
-	return api
+	for _, opt := range opts {
+		opt(c)
+	}
+
+	return c
 }
 
-func (c *Client) Request(method string, path string, body map[string]interface{}) (map[string]interface{}, error) {
+func (c *Client) newRequest(method string, path string, body Body) (*http.Request, error) {
 	u := fmt.Sprintf("%s/%s", c.baseUrl, path)
 	b, err := []byte(nil), error(nil)
 	if body != nil {
@@ -48,48 +98,163 @@ func (c *Client) Request(method string, path string, body map[string]interface{}
 	}
 	req.Header.Add("X-Shopify-Access-Token", c.accessToken)
 	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("Accept", "application/json")
 
-	res, err := c.client.Do(req)
+	return req, nil
+}
+
+func (c *Client) rest(method string, path string, body Body) (Body, error) {
+	threshold := 2 // rate limit threshold
+	req, err := c.newRequest(method, path, body)
 	if err != nil {
 		return nil, err
 	}
-	defer res.Body.Close()
+	result := make(map[string]interface{})
 
-	r := make(map[string]interface{})
-	if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
+	for t := 1; t <= c.tries; t++ {
+		res, err := c.client.Do(req)
+		if err != nil {
+			return nil, err
+		}
+		defer res.Body.Close()
+
+		if res.StatusCode == http.StatusTooManyRequests {
+			retryAfter := res.Header.Get("Retry-After")
+			if t == c.tries {
+				return nil, ErrRateLimit
+			}
+			r, _ := strconv.Atoi(retryAfter)
+			time.Sleep(time.Second * time.Duration(r))
+			continue
+		}
+
+		limit := res.Header.Get("X-Shopify-Shop-Api-Call-Limit")
+		s := strings.Split(limit, "/")
+		bucketSize, _ := strconv.Atoi(s[1])
+		requestCount, _ := strconv.Atoi(s[0])
+		c.availableLimit = bucketSize - requestCount
+
+		if c.availableLimit < threshold {
+			time.Sleep(time.Second * 2)
+			continue
+		}
+
+		if err := json.NewDecoder(res.Body).Decode(&result); err != nil {
+			return nil, err
+		}
+	}
+	return result, nil
+}
+
+// checks if a graphql response has a rate limit error and return it if exists
+func (c *Client) rateLimitError(errors []interface{}) error {
+	for _, err := range errors {
+		if ext, ok := err.(map[string]interface{}); ok {
+			if e, ok := ext["extensions"].(map[string]interface{}); ok {
+				code, ok := e["code"].(string)
+				if ok && (code == "MAX_COST_EXCEEDED" || code == "THROTTLED") {
+					return ErrRateLimit
+				}
+			}
+		}
+	}
+	return nil
+}
+
+// get the current available rate limit from the graphql response
+func (c *Client) graphqlAvailableLimit(extentions map[string]interface{}) int {
+	if cost, ok := extentions["cost"].(map[string]interface{}); ok {
+		if throttleStatus, ok := cost["throttleStatus"].(map[string]interface{}); ok {
+			if currentlyAvailable, ok := throttleStatus["currentlyAvailable"].(float64); ok {
+				return int(currentlyAvailable)
+			}
+		}
+	}
+	return -1
+}
+
+func (c *Client) graphql(body Body) (Body, error) {
+	threshold := 50
+	req, err := c.newRequest(http.MethodPost, "graphql.json", body)
+	if err != nil {
 		return nil, err
 	}
-	return r, nil
+
+	for t := 1; t <= c.tries; t++ {
+		res, err := c.client.Do(req)
+		if err != nil {
+			return nil, err
+		}
+		defer res.Body.Close()
+
+		if res.StatusCode != http.StatusOK {
+			return nil, fmt.Errorf("unexpected server response: %s", res.Status)
+		}
+		result := make(map[string]interface{})
+		dec := json.NewDecoder(res.Body)
+		if err := dec.Decode(&result); err != nil {
+			return nil, err
+		}
+
+		// handle errors
+		if e, ok := result["errors"]; ok {
+			errs := e.([]interface{})
+			err := c.rateLimitError(errs)
+			if t == c.tries {
+				return nil, ErrRateLimit
+			}
+			if err != nil {
+				time.Sleep(2 * time.Second)
+				continue
+			}
+		}
+
+		// check available rate limit
+		if e, ok := result["extensions"]; ok {
+			l := c.graphqlAvailableLimit(e.(map[string]interface{}))
+			if l != -1 {
+				c.availableLimit = l
+			}
+		}
+		if c.availableLimit < threshold {
+			time.Sleep(2 * time.Second)
+			continue
+		}
+		return result["data"].(map[string]interface{}), nil
+	}
+	return nil, nil
 }
 
-func (c *Client) Get(path string) (map[string]interface{}, error) {
-	return c.Request(http.MethodGet, path, nil)
+// Get performs a get request and returns the result
+func (c *Client) Get(path string) (Body, error) {
+	return c.rest(http.MethodGet, path, nil)
 }
 
-func (c *Client) Post(path string, body map[string]interface{}) (map[string]interface{}, error) {
-	return c.Request(http.MethodPost, path, body)
+// post performs a post request and returns the result
+func (c *Client) Post(path string, body Body) (Body, error) {
+	return c.rest(http.MethodPost, path, body)
 }
 
-func (c *Client) Put(path string, body map[string]interface{}) (map[string]interface{}, error) {
-	return c.Request(http.MethodPut, path, body)
+// Put performs a put request and returns the result
+func (c *Client) Put(path string, body Body) (Body, error) {
+	return c.rest(http.MethodPut, path, body)
 }
 
+// Delete performs a delete request
 func (c *Client) Delete(path string) error {
-	_, err := c.Request(http.MethodDelete, path, nil)
+	_, err := c.rest(http.MethodDelete, path, nil)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-// GraphqlQuery performs a graphql request to shopify admin api.
-func (c *Client) GraphqlQuery(query string, variables map[string]interface{}) (map[string]interface{}, error) {
+// Graphql sends a graphql query to shopify admin api.
+func (c *Client) Graphql(query string, variables map[string]interface{}) (Body, error) {
 	body := map[string]interface{}{
 		"query": query,
 	}
 	if variables != nil {
 		body["variables"] = variables
 	}
-	return c.Request(http.MethodPost, "graphql.json", body)
+	return c.graphql(body)
 }
